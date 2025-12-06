fe3fa13f2747c88c8eed0f10ef2342de5fe7627d
Annika Kaul Singh <annikaks@Annikas-MacBook-Pro.local>
2025-12-06 15:29:40 -0800

    globalize number of bins

--- dataset_preparation.py
+++ dataset_preparation.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from config import DATA_FILES, LOGGING, DATASET_PREPARATION_LOGGING
+from config import DATA_FILES, LOGGING, DATASET_PREPARATION_LOGGING, NUM_DISCRETE_CATEGORIES
 
 log = LOGGING or DATASET_PREPARATION_LOGGING
 # return array of prices from csv
@@ -29,11 +29,10 @@ if __name__ == "__main__":
     if log: print(f"dataset_preparation.py:")
     for csv_path in DATA_FILES:
         prices = load_daily_data(csv_path)
-        rets = compute_returns(prices)
+        price_returns = compute_returns(prices)
 
-        cats, bins = discretize_returns_qcut(rets, n_bins=5) # sort into n_bins. 0 -> very bad day ... n_bin - 1 -> very good day 
+        cats, bins = discretize_returns_qcut(price_returns, n_bins=NUM_DISCRETE_CATEGORIES) # sort into n_bins. 0 -> very bad day ... n_bin - 1 -> very good day 
         if log: print(f"     Processing {csv_path}")
-        if log: print("     First 10 categories:", cats[:10])
         if log: print("     Bin edges:", bins)
-        if log: print("     Category counts:", np.bincount(cats))
+        if log: print(f"     Category counts:{np.bincount(cats)}\n")
     if log: print("!!! COMPLETED")
