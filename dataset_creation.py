6bbdc973345aaaedef14da047979d3309b5a3c22
Annika Kaul Singh <annikaks@Annikas-MacBook-Pro.local>
2025-12-06 15:10:22 -0800

    fix header

--- dataset_creation.py
+++ dataset_creation.py
@@ -1,24 +1,31 @@
 import yfinance as yf
 import pandas as pd
+import csv
+import os
 from config import TICKERS, START_DATE, END_DATE
 
 saved_files = []
 
 for ticker_symbol in TICKERS:
-    ticker_data = yf.Ticker(ticker_symbol)
+    # ticker_data = yf.Ticker(ticker_symbol)
     try:
-        history = ticker_data.history(period="1d", raise_errors=True)
-        if not history.empty:
-            data = yf.download(ticker_symbol, start=START_DATE, end=END_DATE)
-
-            data = data[["Close"]]
+        data = yf.download(ticker_symbol, start=START_DATE, end=END_DATE)
+        if not data.empty and 'Close' in data.columns:
             out_path = f"data/{ticker_symbol}_daily.csv"
-            data.to_csv(out_path, index_label="Date")
+            if os.path.exists(out_path):
+                os.remove(out_path)
+            
+            data_to_write = data.reset_index()[['Date', 'Close']]
+            header = ['Date', 'Cost']
+            with open(out_path, mode='w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(header) 
+                writer.writerows(data_to_write.values)
+
 
             print(f"saving {ticker_symbol} data to {out_path}: {len(data)} data loaded")
             saved_files.append(out_path)
-        else:
-            print(f"{ticker_symbol}: no history")
+        
     except Exception as e:
         print(f"{ticker_symbol} not available")
         print(f"    {e}")
