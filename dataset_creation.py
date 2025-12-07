094e7fe746e112b1238c808610ac3ea422129bf6
Annika Kaul Singh <annikaks@Annikas-MacBook-Pro.local>
2025-12-06 16:20:32 -0800

    change file to have callable function

--- dataset_creation.py
+++ dataset_creation.py
@@ -7,35 +7,40 @@ from config import TICKERS, START_DATE, END_DATE, LOGGING, DATASET_CREATION_LOGG
 saved_files = []
 log = LOGGING or DATASET_CREATION_LOGGING
 
-if log: print("dataset_creation.py")
-for ticker_symbol in TICKERS:
-    
-    # ticker_data = yf.Ticker(ticker_symbol)
-    try:
-        data = yf.download(ticker_symbol, start=START_DATE, end=END_DATE)
-        if not data.empty and 'Close' in data.columns:
-            out_path = f"data/{ticker_symbol}_daily.csv"
-            if os.path.exists(out_path):
-                os.remove(out_path)
-            
-            data_to_write = data.reset_index()[['Date', 'Close']]
-            header = ['Date', 'Cost']
-            with open(out_path, mode='w', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerow(header) 
-                writer.writerows(data_to_write.values)
+def parse_and_save_data():
+    if log: print("dataset_creation.py")
+    for ticker_symbol in TICKERS:
+        # ticker_data = yf.Ticker(ticker_symbol)
+        try:
+            data = yf.download(ticker_symbol, start=START_DATE, end=END_DATE)
+            if not data.empty and 'Close' in data.columns:
+                out_path = f"data/{ticker_symbol}_daily.csv"
+                if os.path.exists(out_path):
+                    os.remove(out_path)
+                
+                data_to_write = data.reset_index()[['Date', 'Close']]
+                header = ['Date', 'Cost']
+                with open(out_path, mode='w', newline='') as file:
+                    writer = csv.writer(file)
+                    writer.writerow(header) 
+                    writer.writerows(data_to_write.values)
 
-            if log: print(f"        saving {ticker_symbol} data to {out_path}: {len(data)} data loaded")
-            saved_files.append(out_path)
+                if log: print(f"        saving {ticker_symbol} data to {out_path}: {len(data)} data loaded")
+                saved_files.append(out_path)
 
-    except Exception as e:
-        if log: print(f"        {ticker_symbol} not available")
-        if log: print(f"            {e}")
-if log: print("!!! COMPLETED")
-# write data path files to dynamic .py file (saves file names)
-with open("valid_data_paths.py", "w") as f:
-    f.write("# AUTO-GENERATED. DO NOT EDIT.\n")
-    f.write("DATA_FILES = [\n")
-    for fp in saved_files:
-        f.write(f"    '{fp}',\n")
-    f.write("]\n")
+        except Exception as e:
+            if log: print(f"        {ticker_symbol} not available")
+            if log: print(f"            {e}")
+
+    # write data path files to dynamic .py file (saves file names)
+    with open("valid_data_paths.py", "w") as f:
+        f.write("# AUTO-GENERATED. DO NOT EDIT.\n")
+        f.write("DATA_FILES = [\n")
+        for fp in saved_files:
+            f.write(f"    '{fp}',\n")
+        f.write("]\n")
+
+if __name__ == "__main__":
+    if log: print(f"dataset_creation.py:")
+    parse_and_save_data()
+    if log: print("!!! COMPLETED")
